091f26ee6ff2f2bf5fe28a3bffacc8bf13564552
grburgess <jmichaelburgess@gmail.com>
2021-01-13 10:36:59 +0100

    fix cmdline

--- gbm_kitty/scripts/run_luigi.py
+++ gbm_kitty/scripts/run_luigi.py
@@ -6,8 +6,9 @@ from gbm_kitty.database.create import build_primary_mongo_database
 
 @click.command()
 @click.option('--n_grbs', default=-1, help="build n_grbs", required=True)
-def build_database_data(n_grbs):
-    return build_catalog(n_grbs)
+@click.option('--port', default='8823', help="the port that luigi is on")
+def build_database_data(n_grbs, port):
+    return build_catalog(n_grbs, port)
 
 @click.command()
 def build_database():
